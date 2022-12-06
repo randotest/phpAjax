3f32c3b0f31b71ef1c0e4344b027b9116570454c
randotest <45302424+randotest@users.noreply.github.com>
2022-12-06 17:33:09 +1100

    Update serve.php

--- serve.php
+++ serve.php
@@ -1,22 +1,29 @@
-<?php 
+<?php
 require 'dbconn.php';
 
-$fn=$_POST['firstName'];
-$ln=$_POST['lastName'];
-$ph=$_POST['phone'];
+$req = $_SERVER['REQUEST_METHOD'];
 
-$result = $db->query("select * from test where firstName = '$fn' and lastName = '$ln'");
-$rc = $result->num_rows;
+if ($req === 'POST') {
+	$fn = $_POST['firstName'];
+	$ln = $_POST['lastName'];
+	$ph = $_POST['phone'];
 
-if($rc<=0){
-	$jse = array('error'=>'no values found');
-	exit(1);
-}else{
-	$row = $result->fetch_assoc();
-	/*$jse = array('firstName' => $GLOBALS['fn'], 'lastName' => $GLOBALS['ln'], 'phone' => $GLOBALS['ph']);*/
-	$jse = array('firstName' => $row['firstName'], 'lastName' => $row['lastName'], 'phone' => $row['phone']);
+	$result = $db->query("select * from test where firstName = '$fn' and lastName = '$ln'");
+	$rc = $result->num_rows;
+
+	if ($rc <= 0) {
+		$jse = array('error' => 'no values found');
+		echo json_encode($jse);
+		http_response_code(500);
+		$db->close();
+		exit(1);
+	} else {
+		$row = $result->fetch_assoc();
+		/*$jse = array('firstName' => $GLOBALS['fn'], 'lastName' => $GLOBALS['ln'], 'phone' => $GLOBALS['ph']);*/
+		$jse = array('firstName' => $row['firstName'], 'lastName' => $row['lastName'], 'phone' => $row['phone']);
+		echo json_encode($jse);
+	}
 }
+##contune else if for other request types
 
 $db->close();
-
-echo json_encode($jse);
